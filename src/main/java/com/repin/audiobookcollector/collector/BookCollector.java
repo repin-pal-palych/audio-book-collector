6f093350a91255f6be840aeefd56a765f1f015c7
Pavel Repin <repinpalpalich@gmail.com>
2025-06-02 02:19:15 +0100

    Remove unused method

--- src/main/java/com/repin/audiobookcollector/collector/BookCollector.java
+++ src/main/java/com/repin/audiobookcollector/collector/BookCollector.java
@@ -2,10 +2,7 @@ package com.repin.audiobookcollector.collector;
 
 import com.repin.audiobookcollector.model.Book;
 import com.repin.audiobookcollector.parser.BookCollectorResolver;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 
-import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
@@ -24,17 +21,6 @@ public class BookCollector {
             return new ArrayList<>(collector.collectPages(url, pagesToLoad));
         } catch (URISyntaxException e) {
             throw new RuntimeException("Не получилось распарсить книги: " + e.getMessage());
-        }
-    }
-
-    private static Document loadPage(String url) {
-        try {
-            return Jsoup.connect(url)
-                .userAgent("Mozilla/5.0")
-                .timeout(10000)
-                .get();
-        } catch (IOException e) {
-            throw new RuntimeException("Не удалось загрузить страницу, url: " + url + " error: " + e.getMessage());
         }
     }
 }
